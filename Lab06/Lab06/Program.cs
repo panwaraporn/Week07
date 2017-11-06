b83c4140c935edb36e0be6331c3c4aa4dd31646a
panwaraporn <58030219@kmitl.ac.th>
2017-11-06 21:14:27 +0700

    commite lab06P01-1

---
+++ Lab06/Lab06/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                Student su = new Student();
+                su.Name = "Student Name";
+                su.ID = "12345678";
+                su.GPA = 3.5f;
+                Console.WriteLine("Student name : " + su.Name);
+                Console.WriteLine("Student ID   : " + su.ID);
+                Console.WriteLine("Student GPA  : " + su.GPA);
+                Console.ReadLine();
+            }
+        }
+        class Student
+        {
+            public string Name;
+            public string ID;
+            public float GPA;
+        }
+}
