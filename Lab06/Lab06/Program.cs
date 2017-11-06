eb321e7cf6cc59205c728c779916adf5beaa0bf9
panwaraporn <58030219@kmitl.ac.th>
2017-11-06 21:18:47 +0700

    commit lab06P01-3

--- Lab06/Lab06/Program.cs
+++ Lab06/Lab06/Program.cs
@@ -22,8 +22,9 @@ namespace Lab06
         }
         class Student
         {
-            public string Name;
-            public string ID;
-            public float GPA;
-        }
+        private string Name;
+        private string ID;
+        private float GPA;
+
+    }
 }
