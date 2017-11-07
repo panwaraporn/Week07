bde39bff82300f28e7a4057c79682d18aa2111e6
panwaraporn <58030219@kmitl.ac.th>
2017-11-07 23:44:50 +0700

    commit lab06P03-2

--- Lab06/Lab06/Program.cs
+++ Lab06/Lab06/Program.cs
@@ -12,6 +12,15 @@ namespace Lab06
 
         class Teacher
         {
+            static void Main(string[] args)
+            {
+                Teacher teacher = new Teacher("Tom", 350f);
+                // teacher work for 20Hr/month
+                Console.WriteLine("{0} charge = {1}", teacher.TypeName(),
+                    teacher.CalculateCharge(20f));
+                Console.ReadLine();
+            }
+
             // constructor (for initial private/protected variables)
             public Teacher(string name, float billingRate)
             {
@@ -31,6 +40,34 @@ namespace Lab06
             private string name;
             protected float billingRate;
         }
+        class Professor : Teacher
+        {
+            private float emolument;  // เงินประจำตำแหน่ง
+
+            public Professor(string name, float billingRate) : base(name, billingRate)
+            {
+            }
+
+            public Professor(string name, float billingRate, float emolument)
+            : this(name, billingRate)
+            {
+                this.emolument = emolument;
+            }
+
+            // new function, because it's different than the base version
+            public new float CalculateCharge(float hours)
+            {
+                if (hours < 1.0F)
+                    hours = 1.0F; // minimum charge.
+                return (hours * billingRate) + emolument;
+            }
+            // new function, because it's different than the base version
+            public new string TypeName()
+            {
+                return ("Professor");
+            }
+        }
+
 
     }
 }
