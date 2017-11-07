8532eb588ff6573cd864d16efda4557a512cc228
panwaraporn <58030219@kmitl.ac.th>
2017-11-08 00:05:46 +0700

    commit lab06P07-1

--- Lab06/Lab06/Program.cs
+++ Lab06/Lab06/Program.cs
@@ -6,23 +6,23 @@ using System.Threading.Tasks;
 
 namespace Lab06
 {
-    static void Main(string[] args)
-    {
-        Person p = new Person();
-        p.SayHi();
-        Student s = new Student();
-        s.SayHi();
-        Person sp = new Student();
-        sp.SayHi();
-        Teacher t = new Teacher();
-        t.SayHi();
-        Person tp = new Teacher();
-        tp.SayHi();
-        Console.ReadKey();
-    }
-
     class Person
     {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.SayHi();
+            Student s = new Student();
+            s.SayHi();
+            Person sp = new Student();
+            sp.SayHi();
+            Teacher t = new Teacher();
+            t.SayHi();
+            Person tp = new Teacher();
+            tp.SayHi();
+            Console.ReadKey();
+        }
+
         public virtual void SayHi()
         {
             Console.WriteLine("Hi I'm a person");
@@ -38,6 +38,13 @@ namespace Lab06
     }
 
     class Teacher : Person
+    {
+        public override void SayHi()
+        {
+            Console.WriteLine("Hi I'm a teacher");
+        }
+    }
+    class Professor : Teacher
     {
         public new void SayHi()
         {
