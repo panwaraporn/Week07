3cd91dc99c8565e458565dbc7e5bd4f264e4f2a0
panwaraporn <58030219@kmitl.ac.th>
2017-11-07 23:57:01 +0700

    commit lab06P06-1

--- Lab06/Lab06/Program.cs
+++ Lab06/Lab06/Program.cs
@@ -6,42 +6,42 @@ using System.Threading.Tasks;
 
 namespace Lab06
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Person p = new Person();
+        p.SayHi();
+        Student s = new Student();
+        s.SayHi();
+        Person sp = new Student();
+        sp.SayHi();
+        Teacher t = new Teacher();
+        t.SayHi();
+        Person tp = new Teacher();
+        tp.SayHi();
+        Console.ReadKey();
+    }
+
+    class Person
+    {
+        public virtual void SayHi()
         {
-            Vector v1 = new Vector(1, 4);
-            Vector v2 = new Vector(8, 5);
-            Vector v3 = v1 + v2;
-            Console.WriteLine("vector v1 = " + v1);
-            Console.WriteLine("vector v2 = " + v2);
-            Console.WriteLine("vector v3 = " + v3);
-            Console.WriteLine("-v3 = " + (-v3));
-            Console.ReadKey();
+            Console.WriteLine("Hi I'm a person");
         }
     }
-    class Vector
-    {
-        public Vector(int x, int y)
-        {
-            this.X = x;
-            this.Y = y;
-        }
-        public int X { get; set; }  // X - accessor (property)
-        public int Y { get; set; }  // Y - accessor (property)
 
-        public override string ToString()
+    class Student : Person
+    {
+        public override void SayHi()
         {
-            return string.Format("({0},{1})", X, Y);
+            Console.WriteLine("Hi I'm a student");
         }
-        // All operator overloads must be public and static.
-        public static Vector operator +(Vector v1, Vector v2) //binary operator +
+    }
+
+    class Teacher : Person
+    {
+        public new void SayHi()
         {
-            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
-        }
-        public static Vector operator -(Vector v1)  //unary operator -
-        {
-            return new Vector(-v1.X, -v1.Y);
+            Console.WriteLine("Hi I'm a teacher");
         }
     }
 
