4e21547bcd3512a64ab88618f448d9dc6fb1b092
panwaraporn <58030219@kmitl.ac.th>
2017-11-08 00:48:05 +0700

    commit lab06P02-3

--- Lab06/Lab06/Program.cs
+++ Lab06/Lab06/Program.cs
@@ -6,20 +6,18 @@ using System.Threading.Tasks;
 
 namespace Lab06
 {
-
     class Program
     {
         static void Main(string[] args)
         {
-            //Student su = new Student("Student Name", "12345678");
-            StudentID su = new StudentID("StudentID", 123456789)
+            Student su = new Student("Student Name", "12345678");
             try
             {
-                //su.Name = "My name";
-                su.salary = 300.0f;
-                //Console.WriteLine("Student name : " + su.Name);
-                Console.WriteLine("StudentID   : " + su.StudentID);
-                Console.WriteLine("salary  : " + su.salary);
+                // su.Name = "My name";
+                su.GPA = 3.5f;
+                Console.WriteLine("Student name : " + su.Name);
+                Console.WriteLine("Student ID   : " + su.ID);
+                Console.WriteLine("Student GPA  : " + su.GPA);
             }
             catch (Exception e)
             {
@@ -30,36 +28,36 @@ namespace Lab06
     }
     class Student
     {
-      //  private string name;
+        private string name;
         private string id;
-        private float salary;
+        private float gpa;
 
-        public Student(string StudentID, string salary)
+        public Student(string Name, string ID)
         {
-            this.StudentID = StudentID;   // assign auto variable to the field
-            this.salary = salary;
+            this.name = Name;   // assign auto variable to the field
+            this.id = ID;
         }
 
-        public string StudentID
+        public string Name
         {
-            get { return StudentID; }
+            get { return name; }
         }
-     //   public string ID
+        public string ID
         {
-       //     get { return id; }
-        //}
-        public float salary
+            get { return id; }
+        }
+        public float GPA
         {
             get
             {
-                return salary;
+                return gpa;
             }
             set
             {
-                if (value > 300.0 && value <= 450.0)
-                    salary = value;
+                if (value > 0.0 && value <= 4.0)
+                    gpa = value;
                 else
-                    throw (new Exception("Error!!!! invalid salary"));
+                    throw (new Exception("Error!!!! invalid GPA"));
             }
         }
     }
